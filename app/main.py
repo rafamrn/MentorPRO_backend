d0bc46a188b6594325881db8528c02a4630196ab
r1m2@GITHUB <rafaelmorenods@gmail.com>
2025-10-23 10:12:21 -0300

    REVISAO DOCKER

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import asyncio
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.config import settings
